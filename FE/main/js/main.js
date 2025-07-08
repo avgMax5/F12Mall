f3eb317090d81a13a4c468dcdc2088fac6194a53
h001 <57217558+hwan001@users.noreply.github.com>
2025-07-08 14:03:00 +0900

    feat: [F12MALL-98] [FE] main 페이지 카드뷰 API 연동

--- FE/main/js/main.js
+++ FE/main/js/main.js
@@ -1,4 +1,7 @@
+import { getAllCoins } from '/hook/trade/getAllCoins.js';
+
 document.addEventListener("DOMContentLoaded", function () {
+    renderCoinList();
 
     // Carousel Slide
     const carousel = document.querySelector(".carousel-track");
@@ -144,3 +147,62 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     }
 });
+
+
+async function renderCoinList() {
+    const coinData = await getAllCoins();
+    const container = document.getElementById("card-viewer");
+    container.innerHTML = "";
+
+    if (!coinData || coinData.length === 0) {
+        container.innerHTML = "<div style='text-align: center; font-family: 'KIMM_B'; font-size: 24px; font-weight: 700; color: #00FF2F;'>코인이 없습니다.</div>";
+        return;
+    }
+
+    coinData.forEach((coin, idx) => {
+        container.innerHTML += createCoinElement(coin, idx);
+    });
+
+    attachJoinEvents();
+}
+
+function createCoinElement(coin, idx) {
+  return `
+    <div class="card-view" data-coin-id="${coin.coin_id}">
+        <div class="card-profile">
+            <div class="card-thumbnail">
+                <img src="${coin.profile_image}" alt="카드 ${idx}번 이미지">
+            </div>
+            <div class="card-info">
+                <div class="info-meta">
+                    <div class="info-coin-name">${coin.coin_name}</div>
+                    <div class="info-author">${coin.user_name}</div>
+                </div>
+                <div class="info-change up">
+                    <span class="change-amount">${coin.change_price}</span>
+                    <span class="change-percent">(${coin.fluctuation_rate}%)</span>
+                </div>
+            </div>
+        </div>
+        <div class="card-price">
+            <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <circle class="icon-circle" cx="12" cy="12" r="12" fill="#00FF2F" />
+                <text x="12" y="16" text-anchor="middle">₩</text>
+            </svg>
+            <span class="price-amount">${coin.current_price}</span>
+        </div>
+    </div>
+  `;
+}
+
+// 각 코인 별 클릭 시 /trade?coinId={coinId} 엔드포인트로 리다이렉트 시켜주는 기능을 추가해줌
+function attachJoinEvents() {
+  const cardView = document.querySelectorAll(".card-view");
+  cardView.forEach(btn => {
+    btn.addEventListener("click", () => {
+        const coinId = btn.getAttribute("data-coin-id");
+        window.location.href = `/trade?coinId=${coinId}`;
+    });
+  });
+}
+
