1758ea371c3c8f28463983dc17f6b08e1c42bf06
hwan001 <woghks7209@gmail.com>
2025-07-09 00:00:14 +0900

    feat: [F12MALL] [BE] 종가 테이블 갱신 스케줄러 작성

---
+++ BE/src/main/java/com/avgmax/config/AppConfig.java
@@ -0,0 +1,13 @@
+package com.avgmax.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling 
+@ComponentScan(basePackages = "com.avgmax")
+public class AppConfig {
+
+}
